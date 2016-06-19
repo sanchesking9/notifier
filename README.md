e8c402e73803c50aaa76d5a158e07d7b7187e4c4
Alex Sinner <sancheskinghtc@gmail.com>
2016-06-19 21:12:50 +0300

    Gulpfile
    Added gulp tasks for css, html and js files.

---
+++ README.md
@@ -0,0 +1,8 @@
+#Notifier
+
+### start
+`npm i`
+
+`gulp`
+
+If gulp returns error try to exec this command `export PATH=./node_modules/.bin:$PATH`
---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const del = require('del');
+const newer = require('gulp-newer');
+const debug = require('gulp-debug');
+const browserSync = require('browser-sync').create();
+const notify = require('gulp-notify');
+const browserify = require('browserify');
+const babelify = require('babelify');
+const source = require('vinyl-source-stream');
+
+gulp.task(function css() {
+  return gulp.src('app/css/main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .on('error', notify.onError(function (error) {
+      return {
+        title: 'sass',
+        message: error.message
+      }
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('public/css'));
+});
+
+gulp.task(function assets() {
+  return gulp.src('assets/**', {since: gulp.lastRun('assets')})
+    .pipe(newer('public'))
+    .pipe(debug({title: 'assets'}))
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task(function clean() {
+  return del('public')
+});
+
+gulp.task('bundle', function () {
+  return browserify({entries: 'app/js/app.jsx', extensions: ['.jsx'], debug: true})
+    .transform('babelify', {presets: ['es2015', 'react']})
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('public/js'));
+});
+
+gulp.task(function watch() {
+  gulp.watch('app/**/*.scss', gulp.series('css'));
+  gulp.watch('app/**/*.jsx', gulp.series('bundle'));
+  gulp.watch('assets/**', gulp.series('assets'));
+});
+
+gulp.task('webserver', function() {
+  browserSync.init({
+    server: './public'
+  });
+
+  browserSync.watch('public/**/*.*').on('change', browserSync.reload);
+});
+
+gulp.task('default', gulp.series(gulp.parallel('assets', 'css', 'bundle'), gulp.parallel('webserver', 'watch')));
+
+gulp.task('build', gulp.series('clean', gulp.parallel('assets', 'css'), 'watch'));
